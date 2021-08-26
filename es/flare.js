4797565186356d1f4a6780060e96e310f3600fea
kev1nzh <kev1nzh@app-ark.com>
2021-08-26 16:44:38 +0800

    fix

---
+++ es/flare.js
@@ -0,0 +1,8 @@
+var a = 'first';
+var test = function (value) { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        console.log(value);
+        return [2 /*return*/];
+    });
+}); };
+test(a);
--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,30 @@
 // rollup.config.js
 import typescript from '@rollup/plugin-typescript';
 import babel from '@rollup/plugin-babel';
-export default {
-        input: 'src/index.ts',
-        output: {
-                dir: 'lib',
-                format: 'esm'
+export default [
+        {
+                input: 'src/index.ts',
+                output: {
+                        format: 'esm',
+                        file: 'es/flare.js'
+                },
+                plugins: [
+                        typescript({ resolveJsonModule: true }),
+                        babel({ babelHelpers: 'bundled' })
+
+                ]
         },
-        plugins: [
-                babel({ babelHelpers: 'bundled' }),
-                typescript()
-        ]
-};
+        {
+                input: 'src/index.ts',
+                output: {
+                        format: 'cjs',
+                        file: 'lib/flare.js',
+                        sourcemap: true,
+                        sourcemapFile: 'lib/flare.min.js.map',
+                },
+                plugins: [
+                        typescript({ resolveJsonModule: true }),
+                        babel({ babelHelpers: 'bundled' })
+                ]
+        }
+]
